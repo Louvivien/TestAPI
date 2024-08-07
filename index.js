2d3d425ea6cf64ebae6c80862838535ef1d10ac2
Vivien Richaud <vivienrichaud@Viviens-MacBook-Pro.local>
2024-08-07 15:55:54 +0200

    API swagger

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-// index.js
 const fastify = require('fastify')({ logger: true });
+const path = require('path');
+
+// Register Swagger
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/documentation',
+  swagger: {
+    info: {
+      title: 'User Account API',
+      description: 'An API to provide user account information',
+      version: '1.0.0'
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  }
+});
 
 fastify.get('/api/info', async (request, reply) => {
   return {
@@ -9,9 +26,16 @@ fastify.get('/api/info', async (request, reply) => {
   };
 });
 
+// Serve Swagger UI
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', // optional: default '/'
+});
+
 const start = async () => {
   try {
     await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
+    fastify.swagger(); // Generate the Swagger documentation
     fastify.log.info(`Server is running on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
